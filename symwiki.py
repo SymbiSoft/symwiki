3d6efed1f3aa4b831c3da229be48c7549a6956eb
dbrechalov <dbrechalov@1e0bd318-d289-11dd-8ca8-e3ff79f713b6>
2008-12-28 21:08:58 +0000

    Version 0.2: History and Back button; Create Home page with short help at the first start; Home page menu option; Abuot box.

--- symwiki.py
+++ symwiki.py
@@ -28,32 +28,51 @@ from utils import *
 from xtext import xText
 
 UID = u"e3e34da3"
-VERSION = '0.1'
+VERSION = '0.2'
 
 class WikiEditor(xText):
     version = VERSION
-    title = u('SymWiki %s' % (version))
+    title = u('SymWiki')
     frontpage = 'Home'
     def __init__(self):
         xText.__init__(self)
         self.history = list()
         self.wikidir = 'E:/Wiki'
 
-    def openPage(self, name):
+    def openPage(self, name, noHistory=False):
         if self.fname is not None:
             self.doSave()
-        appuifw2.app.title = u(name)
+            if not noHistory:   # do not add to history if going back
+                self.history.append(self.fname)
+        appuifw2.app.title = u('%s - %s') % (self.title, name)
         self.fname = os.path.join(self.wikidir, name)
         if not os.path.exists(self.fname):
             fn = open(self.fname, 'w')
-            if name != self.frontpage:
-                backlink = '[[%s]]\n' % self.frontpage
+            if name == self.frontpage:
+                txt = '''Welcome to SymWiki - your personal Wiki on S60 device!
+
+This is a Home page, the main page of the Wiki. Replace its contents with your own.
+
+To insert link to another page press *Options*, select *Insert link* and type the name of the page.
+
+To open a link put the cursor on the link between double brackets and press *Select* button.
+'''
             else:
-                backlink = ''
-            fn.write('%s\n%s' % (name, backlink))
+                txt = ''
+            fn.write(txt)
             fn.close()
         self.doOpen()
 
+    def goBack(self):
+        try:
+            name = self.history.pop()
+            self.openPage(name, True)
+        except IndexError:
+            appuifw2.note(u('No previous page available!'))
+
+    def goHome(self):
+        self.openPage(self.frontpage)
+        
     def findLink(self):
         '''Search the [[link]] around cursor
         Return u"text" of the link or None
@@ -71,7 +90,7 @@ class WikiEditor(xText):
         return s(txt)
         
     def insertLink(self):
-        link = appuifw2.query(u('Link'), 'text')
+        link = appuifw2.query(u('Page name'), 'text')
         if link is not None:
             self.editor.add(u('[[%s]]') % link)
     
@@ -79,6 +98,9 @@ class WikiEditor(xText):
         txt = self.findLink()
         if txt is not None:
             self.openPage(txt)
+
+    def aboutDlg(self):
+        appuifw2.query(u('SymWiki\nVersion %s\n(C) Dmitri Brechalov, 2008' % (self.version)), 'query', ok=u(''), cancel=u('Close'))
 
     def quit(self):
         self.doSave()
@@ -90,9 +112,10 @@ class WikiEditor(xText):
         self.app_lock = e32.Ao_lock()
         if not os.path.exists(self.wikidir):
             os.mkdir(self.wikidir)
-        self.openPage(self.frontpage)
+        self.goHome()
         appuifw2.app.menu = [
             (u("Insert link"), self.insertLink),
+            (u("Home page"), self.goHome),
             (u("Edit"), ((u("Undo"), self.undo),
                          (u("Cut"), self.cut),
                          (u("Copy"), self.copy),
@@ -102,10 +125,10 @@ class WikiEditor(xText):
             (u("Search"), ((u("Find Forward"), self.findTextForward),
                            (u("Find Backward"), self.findTextBackward),
                            (u("Replace"), self.replaceText))),
-            (u("About"), self.dummy),
+            (u("About"), self.aboutDlg),
             (u("Exit"), self.quit)
             ]
-        self.bindExitKey((u('Back'), self.dummy))
+        self.bindExitKey((u('Back'), self.goBack))
         self.bindSelectKey(self.clickEvent)
         self.editor.has_changed = False
         e32.ao_yield()
