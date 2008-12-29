740f030899bd6ef4924722ed572368a7b6c281f3
dbrechalov <dbrechalov@1e0bd318-d289-11dd-8ca8-e3ff79f713b6>
2008-12-29 14:29:29 +0000

    Version 1.0 released Screenshots added

--- makesis.sh
+++ makesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION='0.3'
+VERSION='1.0'
 # make distro
 rm -rf build
 mkdir -p build
@@ -9,3 +9,5 @@ cp -v symwiki.py build/default.py
 # create sis-file
 ../Ensymble/ensymble.py py2sis --uid=0xe3e34da3 --appname="Symbian Wiki" --shortcaption="SymWiki" --version=$VERSION --vendor="D.Mych" --verbose build symwiki-${VERSION}.sis
 rm -rf build
+# create zip-file
+zip symwiki-${VERSION}.zip symwiki.py xtext.py utils.py
